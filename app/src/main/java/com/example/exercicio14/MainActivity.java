8f5ff1d08e399102305638ffdbbf396d7d5e8ab4
janeth535 <janethadelaideairosa@gmail.com>
2021-05-11 21:08:55 +0100

    Exercicio14!

---
+++ app/src/main/java/com/example/exercicio14/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.exercicio14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+            }
+        },5000);
+    }
+}
---
+++ app/src/main/java/com/example/exercicio14/MainActivity2.java
@@ -0,0 +1,22 @@
+package com.example.exercicio14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+    public void onClick (View view){
+        Intent i = new Intent (getApplicationContext(),MainActivity.class);
+        startActivity(i);
+    }
+}
